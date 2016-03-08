3febedf63f45691fc743fea6478a558c175ebc28
Frey Hargarten <harga012@umn.edu>
2016-03-07 19:37:13 -0600

    Update scripts.js

--- _base/js/scripts.js
+++ _base/js/scripts.js
@@ -25,13 +25,7 @@ if ($("#gallery").length) { spitFirstGalleries("#gallery"); }
 
   $('a.lightbox').featherlight({ targetAttr: 'href' });
   
-    $('[itemprop=contentUrl]').each(function(i, obj) {
-     $(this).attr("data-size", "" + ($(this).find("img").width() * 1.8) + "x" + ($(this).find("img").height() * 1.8) + "");
-     $(".pswp__img").css("height","");
-  // $(".pswp__img").css("margin-top","10%").css("margin-bottom","10%");
-  // $(".pswp__img, .pswp__zoom-wrap").css("width","100%");
-  // $(".pswp__zoom-wrap").css("transform","");
-    });
+  $("body").append("div");
   
   $(document).bind('DOMNodeInserted', function(event) {
   $('.tmblr-iframe').hide();
