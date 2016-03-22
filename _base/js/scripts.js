3b76361fa9a64ee210361bc3d1ff878f793a6429
Frey Hargarten <harga012@umn.edu>
2016-03-21 21:06:02 -0500

    Update scripts.js

--- _base/js/scripts.js
+++ _base/js/scripts.js
@@ -31,7 +31,9 @@ if ($("#gallery").length) { spitFirstGalleries("#gallery"); }
   $('[itemprop=contentUrl]').each(function(i, obj) {
      $(this).attr("data-size", "" + ($(this).find("img").width() * 1.8) + "x" + ($(this).find("img").height() * 1.8) + "");
      $(".pswp__img").css("height","");
-  //  $(".pswp__zoom-wrap").css("transform","translate3d(4px, 150px, 0px) scale(0.994234)");
+    $(".pswp__zoom-wrap").css("transform","");
+    $(".pswp__zoom-wrap").css("position","absolute");
+    $(".pswp__zoom-wrap").css("top","25%");
   // $(".pswp__img").css("margin-top","10%").css("margin-bottom","10%");
   // $(".pswp__img, .pswp__zoom-wrap").css("width","100%");
   // $(".pswp__zoom-wrap").css("transform","");
@@ -42,7 +44,9 @@ if ($("#gallery").length) { spitFirstGalleries("#gallery"); }
    $('[itemprop=contentUrl]').each(function(i, obj) {
      $(this).attr("data-size", "" + ($(this).find("img").width() * 1.8) + "x" + ($(this).find("img").height() * 1.8) + "");
      $(".pswp__img").css("height","");
-  //   $(".pswp__zoom-wrap").css("transform","translate3d(4px, 150px, 0px) scale(0.994234)");
+     $(".pswp__zoom-wrap").css("transform","");
+     $(".pswp__zoom-wrap").css("position","absolute");
+     $(".pswp__zoom-wrap").css("top","25%");
   // $(".pswp__img").css("margin-top","10%").css("margin-bottom","10%");
   // $(".pswp__img, .pswp__zoom-wrap").css("width","100%");
   // $(".pswp__zoom-wrap").css("transform","");
